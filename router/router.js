71b22d2df9f12d9a7a4a0616e6f6d065c6c669de
Casey <107364140+cdspurlock@users.noreply.github.com>
2023-05-30 11:10:20 -0500

    Updated router to add post, delete, patch, ect. I also added so extra error messages

--- router/router.js
+++ router/router.js
@@ -1,26 +1,117 @@
 const express = require("express");
 const router = express.Router();
 
-// calling http://localhost:3000/exercise
-router.get("/", (req,res)=>{
-    res.status(200).json({
-        message: "Successful GET",
-        metadata: {
-            hostname: req.hostname,
-            method: req.method,
-        }
-    })
+// data store
+let posts = [];
+
+// GET all posts
+router.get("/", (req, res) => {
+  res.status(200).json({
+    message: "Successful GET",
+    metadata: {
+      hostname: req.hostname,
+      method: req.method,
+    },
+    posts: posts,
+  });
 });
 
-router.get("/:id", (req,res,next) => {
-    const id = req.params.id;
+// GET post by ID
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  const post = posts.find((post) => post.id === id);
+
+  if (!post) {
+    res.status(404).json({
+      message: "Post not found",
+      metadata: {
+        hostname: req.hostname,
+        method: req.method,
+      },
+    });
+  } else {
     res.status(200).json({
-        message: 'Successful - GET by ID',
-        id: id,
-        metadata: {
-            hostname: req.hostname,
-            method: req.method,
-        }
-    })
+      message: "Successful - GET by ID",
+      post: post,
+      metadata: {
+        hostname: req.hostname,
+        method: req.method,
+      },
+    });
+  }
 });
+
+// POST a new post
+router.post("/", (req, res) => {
+  const newPost = req.body;
+  posts.push(newPost);
+
+  res.status(201).json({
+    message: "Post created",
+    post: newPost,
+    metadata: {
+      hostname: req.hostname,
+      method: req.method,
+    },
+  });
+});
+
+// PATCH (update) post by ID
+router.patch("/:id", (req, res) => {
+  const id = req.params.id;
+  const updatedPost = req.body;
+  const index = posts.findIndex((post) => post.id === id);
+
+  if (index === -1) {
+    res.status(404).json({
+      message: "Post not found",
+      metadata: {
+        hostname: req.hostname,
+        method: req.method,
+      },
+    });
+  } else {
+    posts[index] = {
+      ...posts[index],
+      ...updatedPost,
+    };
+
+    res.status(200).json({
+      message: "Post updated",
+      post: posts[index],
+      metadata: {
+        hostname: req.hostname,
+        method: req.method,
+      },
+    });
+  }
+});
+
+// DELETE post by ID
+router.delete("/:id", (req, res) => {
+  const id = req.params.id;
+  const index = posts.findIndex((post) => post.id === id);
+
+  if (index === -1) {
+    res.status(404).json({
+      message: "Post not found",
+      metadata: {
+        hostname: req.hostname,
+        method: req.method,
+      },
+    });
+  } else {
+    const deletedPost = posts.splice(index, 1);
+
+    res.status(200).json({
+      message: "Post deleted",
+      post: deletedPost[0],
+      metadata: {
+        hostname: req.hostname,
+        method: req.method,
+      },
+    });
+  }
+});
+
 module.exports = router;
